00d355ea1fd263034754fc9fe627d93cf4b33a3a
YangJiaQi <1295585769@qq.com>
2018-07-31 11:24:30 +0800

    1

---
+++ Assets/daima/Camers.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Camers : MonoBehaviour {
+    private Transform target;
+    Vector3 offset;
+    Camera mycamera;
+    private void Awake()
+    {
+        target = GameObject.FindGameObjectWithTag("Player").transform;
+        mycamera = this.GetComponent<Camera>();
+    }
+    private void Start()
+    {
+        this.transform.position = new Vector3(target.position.x, target.position.y, -10);
+        mycamera.orthographicSize = 2;
+        offset = this.transform.position - target.position;
+    }
+    private void LateUpdate()
+    {
+        this.transform.position = offset + target.position;
+    }
+}
---
+++ Assets/daima/InputManager.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InputManager : MonoBehaviour
+{
+    void Update()
+    {
+        KongZhi();
+    }
+    float x;
+    float y;
+    /// <summary>
+    /// 键盘控制
+    /// </summary>
+    void KongZhi()
+    {
+        if (Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.S) ||
+            Input.GetKey(KeyCode.LeftArrow) || Input.GetKey(KeyCode.RightArrow) || Input.GetKey(KeyCode.UpArrow) || Input.GetKey(KeyCode.DownArrow))
+        {
+            x = Input.GetAxis("Horizontal");
+            y = Input.GetAxis("Vertical");
+            WanJia.Instance.animator.SetBool("Move", true);
+            WanJia.Instance.transform.Translate(new Vector3(x * Time.deltaTime, 0, 0));
+            WanJia.Instance.transform.Translate(new Vector3(0, y * Time.deltaTime, 0));
+        }
+        else
+        {
+            WanJia.Instance.animator.SetBool("Move", false);
+        }
+        WanJia.Instance.animator.SetFloat("X", x);
+        WanJia.Instance.animator.SetFloat("Y", y);
+    }
+}
