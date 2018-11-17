3c90069f3d9b0f0cb6d6960a2fb199efb764172a
YangYuChe <39543734+YangYuChe@users.noreply.github.com>
2018-11-17 17:26:37 +0800

    11
    123131

--- Assets/daima/DiTuManager.cs
+++ Assets/daima/DiTuManager.cs
@@ -7,20 +7,20 @@ public class DiTuManager : MonoBehaviour {
     public int kuan;
     [Header("高")]
     public int gao;
-    [SerializeField]
-    [Header("地图的边框")]
-    private List<GameObject> bianJing;
+    //[SerializeField]
+    //[Header("地图的边框")]
+    //private List<GameObject> bianJing;
     [SerializeField]
     [Header("背景")]
     private GameObject BJ;
     //边框父物体
-    private Transform bianjing;
-    //背景父物体
+    //private Transform bianjing;
+    ////背景父物体
     private Transform beijing;
     private void Awake()
     {
         //获取边框父物体
-        bianjing = this.transform.Find("bianjing");
+        //bianjing = this.transform.Find("bianjing");
         //获取背景父物体
         beijing = this.transform.Find("BJ");
     }
@@ -44,28 +44,28 @@ public class DiTuManager : MonoBehaviour {
         }
         #endregion
         #region 生成边框
-        for (int i = 0; i < kuan; i++)
-        {
-            //中上边框
-            Instantiate(bianJing[4], new Vector3(i, gao + 0.25f, 0), Quaternion.identity, beijing);
-            //中下边框
-            Instantiate(bianJing[5], new Vector3(i, -1.25f, 0), Quaternion.identity, beijing);
-        }
-        for (int j = 0; j < gao; j++)
-        {
-            //中左边框
-            Instantiate(bianJing[6], new Vector3(-1.25f, j, 0), Quaternion.identity, beijing);
-            //中右边框
-            Instantiate(bianJing[7], new Vector3(kuan + 0.25f, j, 0), Quaternion.identity, beijing);
-        }
-        //左上边框
-        Instantiate(bianJing[0], new Vector3(-1.25f, gao + 0.25f, 0), Quaternion.identity, beijing);
-        //左下边框
-        Instantiate(bianJing[1], new Vector3(-1.25f, -1.25f, 0), Quaternion.identity, beijing);
-        //右上边框
-        Instantiate(bianJing[2], new Vector3(kuan + 0.25f, gao + 0.25f, 0), Quaternion.identity, beijing);
-        //右下边框
-        Instantiate(bianJing[3], new Vector3(kuan + 0.25f, -1.25f, 0), Quaternion.identity, beijing);
+        //for (int i = 0; i < kuan; i++)
+        //{
+        //    //中上边框
+        //    Instantiate(bianJing[4], new Vector3(i, gao + 0.25f, 0), Quaternion.identity, beijing);
+        //    //中下边框
+        //    Instantiate(bianJing[5], new Vector3(i, -1.25f, 0), Quaternion.identity, beijing);
+        //}
+        //for (int j = 0; j < gao; j++)
+        //{
+        //    //中左边框
+        //    Instantiate(bianJing[6], new Vector3(-1.25f, j, 0), Quaternion.identity, beijing);
+        //    //中右边框
+        //    Instantiate(bianJing[7], new Vector3(kuan + 0.25f, j, 0), Quaternion.identity, beijing);
+        //}
+        ////左上边框
+        //Instantiate(bianJing[0], new Vector3(-1.25f, gao + 0.25f, 0), Quaternion.identity, beijing);
+        ////左下边框
+        //Instantiate(bianJing[1], new Vector3(-1.25f, -1.25f, 0), Quaternion.identity, beijing);
+        ////右上边框
+        //Instantiate(bianJing[2], new Vector3(kuan + 0.25f, gao + 0.25f, 0), Quaternion.identity, beijing);
+        ////右下边框
+        //Instantiate(bianJing[3], new Vector3(kuan + 0.25f, -1.25f, 0), Quaternion.identity, beijing);
         #endregion
     }
 }
